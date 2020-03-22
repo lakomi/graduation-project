0a7525d784d04b2385ab38e4898fe5393bcc89b6
AHRL <13521818975@163.com>
2020-03-22 20:13:24 +0800

    2020.3.22-1

---
+++ src/main/java/com/edu/graduation/service/StatisticService.java
@@ -0,0 +1,16 @@
+package com.edu.graduation.service;
+
+import com.edu.graduation.entity.dto.SellTotalDTO;
+import com.edu.graduation.vo.ResultVo;
+
+public interface StatisticService {
+    /**
+     * 获取店铺每天的营业额
+     * @param sellTotalDTO
+     * @return
+     */
+    ResultVo everyDaySellTotal(SellTotalDTO sellTotalDTO);
+
+    ResultVo everyDayFoodSellCount();
+
+}
